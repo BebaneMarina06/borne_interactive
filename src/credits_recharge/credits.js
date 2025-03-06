ae61e2cd7e7d56a807e0e6a66eb7e36617f4d108
Cabrelboukamba <Cabrelboukamba@gmail.com>
2025-03-06 11:49:10 +0100

    ajout de la redirection vers la page app.js

--- src/credits_recharge/credits.js
+++ src/credits_recharge/credits.js
@@ -1,45 +1,45 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import pour la navigation
 import './css/credits.css';
 import artelLogo from '../assets/images/airtel.png';
 import moovLogo from '../assets/images/moov.png';
 
-
 const Credits = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
+  const navigate = useNavigate(); // Hook pour la navigation
 
   const handleNumberClick = (number) => {
     setPhoneNumber(prev => prev + number);
   };
 
   const handleValidate = () => {
-    alert(`Numéro ${phoneNumber} rechargé avec succès!`);  
-     setPhoneNumber('');
+    alert(`Numéro ${phoneNumber} rechargé avec succès!`);
+    setPhoneNumber('');
   };
 
   const handleCancel = () => {
-    setPhoneNumber('');
+    navigate('/'); // Redirige vers la page principale (App.js)
   };
 
   return (
     <div className="recharge-interface">
       <div className="titre">
-      <h1>Crédit de communication</h1>
-      <div class="airtel-moov">
-                  <img src={artelLogo} alt="Logo-airtel" />
-                  <img src={moovLogo} alt="Logo-moov" />
-                </div>
+        <h1>Crédit de communication</h1>
+        <div className="airtel-moov">
+          <img src={artelLogo} alt="Logo-airtel" />
+          <img src={moovLogo} alt="Logo-moov" />
+        </div>
       </div>
 
       <div className="form-container">
         {/* Input du numéro */}
         <div className="phone-number">
-        <p><strong>1</strong> - Entrer le numéro de téléphone à recharger</p>
+          <p><strong>1</strong> - Entrer le numéro de téléphone à recharger</p>
           <input type="text" value={phoneNumber} readOnly placeholder="Numéro de téléphone" />
           
           <p><strong>2</strong> - Confirmez le numéro de téléphone</p>
           <input type="text" value={phoneNumber} readOnly placeholder="Numéro de téléphone" />
         </div>
-        
 
         {/* Clavier numérique */}
         <div className="keypad">
@@ -56,11 +56,6 @@ const Credits = () => {
         <button onClick={handleCancel}>Annuler</button>
         <button onClick={handleValidate}>Valider</button>
       </div>
-
-      {/* Nom du fournisseur
-      <div className="provider">
-        <p>Gabon Telecom</p>
-      </div> */}
     </div>
   );
 };
